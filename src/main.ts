f8a38bab56993dfc5edd6f29f63c035d7a46ffea
Frédéric Dupérier <fduperier@gmail.com>
2016-07-27 00:29:50 +0200

    Utilisation du nouveau module forms

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 import { AppComponent, environment } from './app/';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  disableDeprecatedForms(),
+  provideForms()
+])
+.catch((err: any) => console.error(err));
