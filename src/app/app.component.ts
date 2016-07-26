3c6c5f0266a9af134f2548ed081e9baee974f34a
Frédéric Dupérier <fduperier@gmail.com>
2016-07-27 00:31:28 +0200

    Création du composant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { PostFormComponent } from './posts/post-form';
+
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [ PostFormComponent ]
 })
 export class AppComponent {
   title = 'app works!';
---
+++ src/app/posts/post-form/post-form.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Post } from '../shared/post.model';
+
+@Component({
+  moduleId: module.id,
+  selector: 'post-form',
+  templateUrl: 'post-form.component.html',
+  styleUrls: ['post-form.component.css']
+})
+export class PostFormComponent {
+  categories = [ 'Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5' ];
+
+  model = new Post(1,
+                   'This is my post title',
+                   'Lorem ipsum dolor sit amet...',
+                   this.categories[0],
+                   'My tag');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // Only for testing
+  get diagnostic() { return JSON.stringify(this.model); }
+}
