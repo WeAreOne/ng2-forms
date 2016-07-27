30d873ccdf6cf083851d44677bd29444ec9778d5
Frédéric Dupérier <fduperier@gmail.com>
2016-07-27 23:13:47 +0200

    Formulaire dans le contrôleur : création du formulaire

--- src/app/posts/other-post-form/other-post-form.component.ts
+++ src/app/posts/other-post-form/other-post-form.component.ts
@@ -14,8 +14,16 @@ export class OtherPostFormComponent {
   categories = [ 'Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5' ];
   model = new Post(1, 'This is my post title', 'Lorem ipsum dolor sit amet...', this.categories[0], 'My tag');
   submitted = false;
+  postForm: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {
+    this.postForm = fb.group({
+      title: fb.control(this.model.title, Validators.required),
+      text: fb.control(this.model.text, Validators.required),
+      category: fb.control(this.model.category),
+      tag: fb.control(this.model.tag)
+    })
+  }
 
   onSubmit() { this.submitted = true; }
 }
